3023e91c1f4826e37188d2d7e8e1fae9f36f2056
hectorastrom <hector.astrom@gmail.com>
2025-11-28 17:16:58 -0500

    extract highest freq @ timestep

--- signals/freq_seperation.py
+++ signals/freq_seperation.py
@@ -1,5 +1,47 @@
-import soundfile as sf
-import numpy as np
+# @Time    : 2025-11-28 17:01
+# @Author  : Hector Astrom
+# @Email   : hastrom@mit.edu
+# @File    : freq_seperation.py
+"""
+Seperate the conversational members by their assigned frequencies
+1. STFT the signal 
 
-data, fs = sf.read("audio/test.wav")
+Later:
+- Determine which part of the audio is the calibration (people announcing their
+names) and which is the 'action'
+- Continually update frequency bands for members as they're speaking
+- 
+"""
+import numpy as np
+from scipy.io.wavfile import read
+from scipy.signal import stft
+
+
+###############################
+# Reading file
+###############################
+fs, data = read("audio/test.wav")
 print(fs)
+print(data)
+
+
+###############################
+# STFT
+###############################
+duration = len(data) / fs
+N = duration * fs
+
+nperseg = 1028
+t_granularity = nperseg / fs
+print(f"Segments are {t_granularity:.2f}s long - that's our granularity")
+f, t, Zkk = stft(data, fs, 'hann', nperseg=nperseg, noverlap=256)
+print(f[:5])
+
+# convert k's to frequencies
+desired_time_s = 3 # what exact s you want to look at
+s_idx = int(desired_time_s / t_granularity)
+freq_mags_at_t = np.abs(Zkk[:, s_idx])
+highest_freq_idx = np.argmax(freq_mags_at_t)
+highest_freq = float(f[highest_freq_idx])
+print(f"{highest_freq=:.2f}Hz")
+
